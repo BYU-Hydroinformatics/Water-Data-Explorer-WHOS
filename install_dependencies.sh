b90ffe6884a65525474f61fad12a598088cdc423
romer8 <gio.busrom@gmail.com>
2022-05-13 11:10:01 -0600

    changes to install

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -13,11 +13,12 @@
 # mamba install -c conda-forge --yes --file requirements.txt
 # Install mamba first
 echo ${CONDA_HOME}
-. ${CONDA_HOME}/bin/conda install -c conda-forge --yes mamba
+. ${CONDA_HOME}/bin/install -c conda-forge --yes mamba
 #
 #  To go around issue above, one can
 #  iterate over all lines in the
 #  requirements.txt file.
 #
+. ${CONDA_HOME}/bin/activate tethys
 
 while read requirement; do . ${CONDA_HOME}/bin/mamba install --yes $requirement || . ${CONDA_HOME}/bin/pip install $requirement; done < requirements.txt
