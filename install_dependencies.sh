4ce55dec7c6857253cf00567238842df2661260d
romer8 <gio.busrom@gmail.com>
2022-05-14 15:52:19 -0600

    Pushing new chanes to container

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -14,11 +14,12 @@
 # Install mamba first
 echo ${CONDA_HOME}
 ${CONDA_HOME}/bin/conda install -c conda-forge --yes mamba
+${CONDA_HOME}/bin/mamba install -n tethys pip
 #
 #  To go around issue above, one can
 #  iterate over all lines in the
 #  requirements.txt file.
 #
-. ${CONDA_HOME}/bin/activate tethys
+${CONDA_HOME}/bin/activate tethys
 
-while read requirement; do ${CONDA_HOME}/bin/mamba install --yes $requirement || ${CONDA_HOME}/bin/pip install $requirement; done < requirements.txt
+while read requirement; do ${CONDA_HOME}/bin/mamba install -c conda-forge --yes $requirement || ${CONDA_HOME}/bin/pip install $requirement; done < requirements.txt
